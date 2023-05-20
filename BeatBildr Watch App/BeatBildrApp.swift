664a1db6b1a6b3b18345dbc00e94b9d300ef19e0
AcmeCat <31060311+AcmeCat@users.noreply.github.com>
2023-05-20 11:35:36 +0800

    Initial Commit

---
+++ BeatBildr Watch App/BeatBildrApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatBildrApp.swift
+//  BeatBildr Watch App
+//
+//  Created by Rory Allen on 20/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct BeatBildr_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeatBildr Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BeatBildr Watch App
+//
+//  Created by Rory Allen on 20/5/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
