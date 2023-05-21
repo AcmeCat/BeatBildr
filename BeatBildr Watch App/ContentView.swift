cf14820251811f96514778dd59f39b28f25a0759
AcmeCat <31060311+AcmeCat@users.noreply.github.com>
2023-05-21 17:16:29 +0800

    basic soundboard app features. four buttons/ four sounds. only one avplayer so tapping a button cuts off any other existing sound that is playing

--- BeatBildr Watch App/ContentView.swift
+++ BeatBildr Watch App/ContentView.swift
@@ -6,50 +6,47 @@
 //
 
 import SwiftUI
+import AVFAudio
+
+var player: AVAudioPlayer!
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            HStack {
-                Button ("!", action: playSound1)
-                    .background(Color(.orange))
-                    .cornerRadius(10)
-                    .font(Font(CTFont(.pushButton, size: 32)))
-                Button ("@", action: playSound2)
-                    .background(Color(.cyan))
-                    .cornerRadius(10)
-                    .font(Font(CTFont(.pushButton, size: 32)))
+            HStack { //all these buttons are essentially the same. TODO: They should be extracted into their own custom button view
+                Button(action: {playSound("crash")})
+                {Text("!").font(Font(CTFont(.pushButton, size: 48)))}
+                .background(Color(.orange))
+                .cornerRadius(10)
+                Button(action: {playSound("snare")})
+                {Text("@").font(Font(CTFont(.pushButton, size: 48)))}
+                .background(Color(.cyan))
+                .cornerRadius(10)
             }
             HStack {
-                Button ("#", action: playSound3)
-                    .background(Color(.yellow))
-                    .cornerRadius(10)
-                    .font(Font(CTFont(.pushButton, size: 32)))
-                
+                Button(action: {playSound("hat")})
+                {Text("#").font(Font(CTFont(.pushButton, size: 48)))}
+                .background(Color(.yellow))
                 .cornerRadius(10)
-                Button ("$", action: playSound4)
-                    .background(Color(.magenta))
-                    .cornerRadius(10)
-                    .font(Font(CTFont(.pushButton, size: 32)))
+                Button(action: {playSound("kick")})
+                {Text("$").font(Font(CTFont(.pushButton, size: 48)))}
+                .background(Color(.magenta))
+                .cornerRadius(10)
             }
         }
         .padding()
     }
     
-    func playSound1() {
+    //super inefficient way of doing this, i.e. spinning up new instance of an AVAudioPlayer. TODO: Revise
+    func playSound(_ name: String) {
         print("hello !")
-    }
-    
-    func playSound2() {
-        print("hello @")
-    }
-    
-    func playSound3() {
-        print("hello #")
-    }
-    
-    func playSound4() {
-        print("hello $")
+        guard let url = Bundle.main.url(forResource: name, withExtension: "mp3") else { return }
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.play()
+        } catch {
+            print("error")
+        }
     }
 }
 
